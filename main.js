34a338a3208d51abbbaf3ed2a3a250c37ce82c32
AnnyC28 <160602515+AnnyC28@users.noreply.github.com>
2024-06-20 08:30:59 -0300

    Aula programação e português 01

---
+++ main.js
@@ -0,0 +1,52 @@
+const caixaPrincipal = document.querySelector('.caixaPrincipal');
+const caixaPerguntas = document.querySelector('.caixaPerguntas');
+const caixaAlternativas = document.querySelector('.caixaAlternativas');
+const caixaResultado = document.querySelector('.caixaResultado');
+const textoResultados = document.querySelector('.textoResultados');
+const perguntas = [
+    {
+        enunciado: "Pegunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+
+        ]
+
+    },
+    {
+        enunciado: "Pegunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+
+        ]
+
+    },
+    {
+        enunciado: "Pegunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+
+        ]
+
+    },
+    {
+        enunciado: "Pegunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+
+        ]
+
+    },
+    {
+        enunciado: "Pegunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+
+        ]
+
+    },
+]
